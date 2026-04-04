1a6910e75f66b3c9b042247b7be1b8550745a46e
dharshi-b <dharshi@wecrest.com>
2026-04-04 09:56:54 +0530

    Add cors test

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware')
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: process.env.FRONTEND_URL || "http://localhostt:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     })
 );

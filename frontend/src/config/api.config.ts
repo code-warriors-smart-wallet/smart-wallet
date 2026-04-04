89de3c84d6f74063d698df2c916b6237a7bfc96b
dharshi-b <dharshi@wecrest.com>
2026-04-04 08:40:58 +0530

    Add VITE_API_GATEWAY_BASE_URL to environment configuration

--- frontend/src/config/api.config.ts
+++ frontend/src/config/api.config.ts
@@ -3,7 +3,7 @@ import { logout, setIsAuthenticated, loginSuccess } from '../redux/features/auth
 import store from '../redux/store/store'; // Import your Redux store
 import { toast } from 'react-toastify';
 
-export const API_BASE_URL = "http://localhost:8080/";
+export const API_BASE_URL = import.meta.env.VITE_API_GATEWAY_BASE_URL || "http://localhostt:8080/";
 
 export const API_CONFIG = {
     baseURL: API_BASE_URL,

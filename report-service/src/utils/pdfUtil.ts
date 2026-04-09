d4d88f211f7dd12c9f17529665e63265c582500d
Dharshi Balasubramaniyam <dharshi@wecrest.com>
2026-04-09 19:36:00 +0530

    Debug (#46)
    * Refactor report generation to use Set for unique space names in income vs expense, budget utilization, and loan repayment summary endpoints
    
    * Format net change display to show two decimal places in SpendingSummary component
    
    * Add launch arguments for Puppeteer in getPdfFromHTML function

--- report-service/src/utils/pdfUtil.ts
+++ report-service/src/utils/pdfUtil.ts
@@ -1,7 +1,12 @@
 import puppeteer from "puppeteer";
 
 export async function getPdfFromHTML(html: string): Promise<Uint8Array<ArrayBufferLike>> {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch(
+    { 
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    }
+  );
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: "networkidle0" });

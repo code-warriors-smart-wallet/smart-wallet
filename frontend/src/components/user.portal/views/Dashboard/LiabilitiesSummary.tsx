2cbc79bb24eec3d91091523bec061da1d4de92ba
dharshi-b <dharshi@wecrest.com>
2026-04-09 09:58:48 +0530

    Debug #33

--- frontend/src/components/user.portal/views/Dashboard/LiabilitiesSummary.tsx
+++ frontend/src/components/user.portal/views/Dashboard/LiabilitiesSummary.tsx
@@ -26,7 +26,7 @@ function LiabilitiesSummary({ currency, summary }: { currency: string, summary: 
     }, [space, summary])
 
     return (
-        <section className="app-card *:text-text-light-primary *:dark:text-text-dark-primary">
+        <section className="rounded my-3 py-2 px-3 border border-border-light-primary dark:border-border-dark-primary *:text-text-light-primary *:dark:text-text-dark-primary">
             {/* title */}
             <div className="rounded flex justify-between items-center">
                 <span className="flex gap-3 items-center text-xl font-bold"><MdOutlineRequestQuote />Liabilities summary</span>

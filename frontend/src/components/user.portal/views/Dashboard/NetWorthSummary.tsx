f52522f8b507f9d9caf6d6a61f2976c792023622
Dharshi Balasubramaniyam <dharshi@wecrest.com>
2026-04-09 15:47:11 +0530

    Feature/db/ai (#41)
    * Add ai-service module
    
    (cherry picked from commit 56a19db1c72e8b7ff5fb02c5d429c5c7febe94a6)
    
    * Add environment variable configuration for ai-service
    
    * Fix formatting of net worth display in Dashboard to show two decimal places
    
    ---------
    
    Co-authored-by: Uda <115524136+UdaBG@users.noreply.github.com>

--- frontend/src/components/user.portal/views/Dashboard/NetWorthSummary.tsx
+++ frontend/src/components/user.portal/views/Dashboard/NetWorthSummary.tsx
@@ -6,7 +6,7 @@ function NetWorthSummary({ summary, currency }: { summary: any, currency: string
     return (
         <div className="rounded my-3 p-3 flex justify-between items-center border border-border-light-primary dark:border-border-dark-primary *:text-xl *:text-text-light-primary *:dark:text-text-dark-primary *:font-bold bg-secondary">
             <span className="flex gap-3 items-center"><FaBalanceScale /><span>Net worth (Assets - Liabilities):</span></span>
-            <span>{assets-liabilties} {currency}</span>
+            <span>{(assets-liabilties).toFixed(2)} {currency}</span>
         </div>
     )
 }
